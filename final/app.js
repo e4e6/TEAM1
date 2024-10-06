6057a5e1aaa19e28d7ebd4ce56d4ecc0963a13a2
e4e6 <114649461+e4e6@users.noreply.github.com>
2024-10-07 03:11:12 +0900

    set:project

---
+++ final/app.js
@@ -0,0 +1,29 @@
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+const img = document.getElementById("tab-img");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+
+  if (id === "red") {
+    img.src = "./r.webp"; 
+  } if (id === "green") {
+    img.src = "./g.webp"; 
+  } if (id === "blue") {
+    img.src = "./b.webp"; 
+  }
+});
